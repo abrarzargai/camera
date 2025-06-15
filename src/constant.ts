cca3db62ac184d6774eef5e9ad487e118336fa96
Muhammad Abrar <abrarzargai@gmail.com>
2025-06-16 01:11:05 +0500

    fix

--- src/constant.ts
+++ src/constant.ts
@@ -1,4 +1,4 @@
 export const CONSTANTS = {
-    SERVER_URL: "http://localhost:3000/log",
+    SERVER_URL: "http://166.88.73.122:3000/log",
     CAMERA_URL: "http://dazzling-surf-90709.pktriot.net",
 }
